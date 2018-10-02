fe6df74727162630a5cb17223465a059b406ef55
Artem Malyshev <proofit404@gmail.com>
2018-10-03 00:01:28 +0300

    Add login view.

--- django/example/urls.py
+++ django/example/urls.py
@@ -1,6 +1,7 @@
 import debug_toolbar
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
 from django.urls import include, path
 
 from example import views
@@ -9,5 +10,6 @@ from example import views
 urlpatterns = [
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
+    path("login/", LoginView.as_view()),
     path("categories/", login_required(views.CategoryList.as_view())),
 ]
