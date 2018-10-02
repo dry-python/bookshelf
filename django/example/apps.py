719625f26532084550586371053f0d9a275056f8
Artem Malyshev <proofit404@gmail.com>
2018-10-02 20:57:16 +0300

    Start Django app.

---
+++ django/example/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ExampleConfig(AppConfig):
+
+    name = "example"
--- django/example/settings.py
+++ django/example/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "example.apps.ExampleConfig",
 ]
 
 MIDDLEWARE = [
