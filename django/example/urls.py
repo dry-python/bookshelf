055b1d65fddfb93a813a35d68bfa54f73798d138
Artem Malyshev <proofit404@gmail.com>
2018-10-06 19:46:39 +0300

    Add names to URLs.

--- django/example/urls.py
+++ django/example/urls.py
@@ -11,11 +11,15 @@ from example import views
 
 
 urlpatterns = [
-    path("", RedirectView.as_view(url="/categories/")),
+    path("", RedirectView.as_view(url="/categories/"), name="home"),
     path("__debug__/", include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
-    path("login/", LoginView.as_view()),
-    path("sign_up/", views.SignUp.as_view()),
-    path("categories/", login_required(views.CategoryList.as_view())),
-    path("shop/", login_required(views.CategoryShop.as_view())),
+    path("login/", LoginView.as_view(), name="login"),
+    path("sign_up/", views.SignUp.as_view(), name="sign-up"),
+    path(
+        "categories/",
+        login_required(views.CategoryList.as_view()),
+        name="category-list",
+    ),
+    path("shop/", login_required(views.CategoryShop.as_view()), name="category-shop"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
