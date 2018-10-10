b3f85a8eb1af779309d820081311029eef570a1b
Artem Malyshev <proofit404@gmail.com>
2018-10-10 04:55:08 +0300

    Add logout view to the routing.

--- django/example/urls.py
+++ django/example/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import include, path
 from django.views.generic import RedirectView
 
@@ -16,6 +16,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # TODO: Do not show login for authenticated user.
     path("login/", LoginView.as_view(), name="login"),
+    path(
+        "logout/",
+        login_required(LogoutView.as_view(template_name="registration/logout.html")),
+        name="logout",
+    ),
     # TODO: Do not show sign up for authenticated user.
     path("sign_up/", views.SignUpView.as_view(), name="sign-up"),
     path("profile/", login_required(views.ProfileView.as_view()), name="profile"),
