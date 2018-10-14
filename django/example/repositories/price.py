7435192e5ea09ea426a7282d334d11a6420fa5d1
Artem Malyshev <proofit404@gmail.com>
2018-10-15 01:01:55 +0300

    Correct filter argument.

--- django/example/repositories/price.py
+++ django/example/repositories/price.py
@@ -15,10 +15,23 @@ def prices_for_category(category):
 
 def cheapest_price_by_category(categories):
 
-    # FIXME: Filter by most recent date for each category and period.
+    # FIXME: Express this query.
+    #
+    # SELECT "category_id",
+    #        MIN("cost")
+    #   FROM "example_price"
+    #  WHERE "id" IN (
+    #    SELECT "id"
+    #      FROM "example_price"
+    #     WHERE "category_id" IN (1, 2, 3, 4)
+    #     GROUP BY "category_id", "period"
+    #    HAVING "from_date" = MAX("from_date")
+    #  )
+    #  GROUP BY "category_id"
+
     return {
         each["category"]: each["min_cost"]
-        for each in Price.objects.filter(pk__in=categories)
+        for each in Price.objects.filter(category__in=categories)
         .values("category")
         .annotate(min_cost=Min("cost"))
         .order_by()
