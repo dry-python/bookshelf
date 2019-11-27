c3b4209abd452cf2ba41b2b20d96fa93471287ab
Artem Malyshev <proofit404@gmail.com>
2019-11-28 02:17:04 +0300

    Models should not be used for navigation.

--- bookshelf/bookshelf/models/category.py
+++ bookshelf/bookshelf/models/category.py
@@ -1,11 +1,6 @@
 from django.db import models
-from django.urls import reverse
 
 
 class Category(models.Model):
 
     name = models.CharField(max_length=100)
-
-    def get_absolute_url(self):
-
-        return reverse("category-detail", args=(self.pk,))
